f579707a723a88211633c581481cc1e8a962fede
lagomoura <mouralago@live.com>
2024-05-09 18:37:48 -0300

    new model email_detector running

--- microservicios/routers/address_phone_detector_router.py
+++ microservicios/routers/address_phone_detector_router.py
@@ -1,12 +1,12 @@
 
 # . Controler - Direcciona endpoint al archivo
-from fastapi import APIRouter
-from sql_app.dependencias import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sql_app.models import Image, Tags, ImageTagAssociation
 
-from microservicios.services.address_phone_detector import address_phone_detector
+from microservicios.services.address_phone_detector import \
+    address_phone_detector
+from sql_app.dependencias import get_db
+from sql_app.models import Image, ImageTagAssociation, Tags
 
 router = APIRouter(prefix="/microservicios")
 
@@ -16,33 +16,36 @@ def detector_direccion_telefono(id: str, db: Session = Depends(get_db)):
 
     image = db.query(Image).filter(Image.id == id).first()
     if not image:
-    
+
         raise HTTPException(status_code=404, detail="Imagen no encontrada")
-    
-    
-    image_tag_association = db.query(ImageTagAssociation).filter(ImageTagAssociation.image_id == id).first()
-    
+
+    image_tag_association = db.query(ImageTagAssociation).filter(
+        ImageTagAssociation.image_id == id).first()
+
     if image_tag_association and image_tag_association.detected == True:
-        
+
         return {"message": " El procesamiento de deteccion de direcciones y telefonos ya ha sido realizado sobre esa imagen"}
-        
-        
+
     path = image.path
     address_phone_detectados = address_phone_detector(path)
-    
+
     if address_phone_detectados:
-        
-        servicio_realizado = db.query(Tags).filter(Tags.tag_service == "Address_phone_detected").first()
-        new_image_tag_association = ImageTagAssociation(image_id = id, tags_id = servicio_realizado.id, detected = True)
+
+        servicio_realizado = db.query(Tags).filter(
+            Tags.tag_service == "Address_phone_detected").first()
+        new_image_tag_association = ImageTagAssociation(
+            image_id=id, tags_id=servicio_realizado.id, detected=True)
         db.add(new_image_tag_association)
         db.commit()
-    
+
         return {"message": "Deteccion de direcciones y telefonos realizada exitosamente"}
-    
+
     else:
-        servicio_realizado = db.query(Tags).filter(Tags.tag_service == "Address_phone_detected").first()    
-        new_image_tag_association = ImageTagAssociation(image_id = id, tags_id = servicio_realizado.id, detected = False)
+        servicio_realizado = db.query(Tags).filter(
+            Tags.tag_service == "Address_phone_detected").first()
+        new_image_tag_association = ImageTagAssociation(
+            image_id=id, tags_id=servicio_realizado.id, detected=False)
         db.add(new_image_tag_association)
         db.commit()
-        
+
         return {"message": "No ha sido detectado direcciones y/o nums. telelefonos en la imagen"}
--- microservicios/routers/email_detector_router.py
+++ microservicios/routers/email_detector_router.py
@@ -1,12 +1,11 @@
 
 # . Controler - Direcciona endpoint al archivo
-from fastapi import APIRouter
-from sql_app.dependencias import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sql_app.models import Image
 
 from microservicios.services.email_detector import email_detector
+from sql_app.dependencias import get_db
+from sql_app.models import Image, ImageTagAssociation, Tags
 
 #! Enrutador llamado router con un prefijo de URL "/microservicios
 router = APIRouter(prefix="/microservicios")
@@ -17,28 +16,29 @@ def detectar_email_img(id: str, db: Session = Depends(get_db)):
 
     image = db.query(Image).filter(Image.id == id).first()
 
-    if image:
-        path = image.path
-        email_detectados = email_detector(path)
-        service_tag = ["EMAIL_DETECTOR"]
-
-        if "EMAIL_DETECTOR" not in image.services:
-            image.services = service_tag
-
-        else:
-            return {"message": "El procesamiento de deteccion de emails ya ha sido realizado sobre esa imagen"}
-
-        if email_detectados:
-            email_tag = ["EMAIL_detected"]
-            image.tags = email_tag
-
-            db.add(image)
-            db.commit()
-
-            return {"message": "Deteccion de emails realizada exitosamente"}
-        else:
-            db.add(image)
-            db.commit()
-            return {f"message": "No se detectaron emails en la imagen"}
-    else:
+    if not image:
         raise HTTPException(status_code=404, detail="Imagen no encontrada")
+
+    image_tag_association = db.query(ImageTagAssociation).filter(
+        ImageTagAssociation.image_id == id).first()
+
+    if image_tag_association and image_tag_association.detected == True:
+
+        return {"message": "El procesamiento de deteccion de emails ya ha sido realizado sobre esa imagen"}
+
+    path = image.path
+    email_detectados = email_detector(path)
+
+    if email_detectados:
+        servicio_realizado = db.query(Tags).filter(
+            Tags.tag_service == "Email_detected").first()
+        new_image_tag_association = ImageTagAssociation(
+            image_id=id, tags_id=servicio_realizado.id, detected=True)
+
+        db.add(new_image_tag_association)
+        db.commit()
+
+        return {"message": "Deteccion de emails realizada exitosamente"}
+
+    else:
+        return {f"message": "No se detectaron emails en la imagen"}
